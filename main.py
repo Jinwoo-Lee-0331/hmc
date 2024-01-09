acfd81974f005e9c5e690f3c4d4999aa6da5b58f
Jinlee0331 <131830564+Jinwoo-Lee-0331@users.noreply.github.com>
2024-01-09 16:34:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
 
-st.write("호스트 이름:", hostname)
-st.write("IP 주소:", ip_address)
+st.write("호스트 이름:")
+st.write("IP 주소:")
