2858feb8e1d990b43eea1c4bfd19263f46bb65d1
Jinlee0331 <131830564+Jinwoo-Lee-0331@users.noreply.github.com>
2024-06-04 14:29:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import streamlit as st
 # HTML 파일을 읽어서 HTML 컴포넌트로 출력
 with open('CV - Jinwoo Lee.html', 'r', encoding='utf-8') as file:
     html_code = file.read()
-st.components.v1.html(html_code, width=1000)
+st.components.v1.html(html_code, height=8000, width=1000)
